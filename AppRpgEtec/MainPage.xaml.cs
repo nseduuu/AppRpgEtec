2d80b9cb88a3d53afd336a3787ce25e86fceb430
Eduardo Noronha <eduardo.noronhasampaio@gmail.com>
2024-03-27 22:49:52 -0300

    adding navigation

--- AppRpgEtec/MainPage.xaml.cs
+++ AppRpgEtec/MainPage.xaml.cs
@@ -8,7 +8,15 @@
             InitializeComponent();
         }
 
-        
+        private async void TapMenuDisputas(object sender, TappedEventArgs e)
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new Views.Disputas.ListagemView());
+        }
+
+        private async void TapMenuPersonagens(object sender, TappedEventArgs e)
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new Views.Personagens.ListagemView());
+        }
     }
 
 }
